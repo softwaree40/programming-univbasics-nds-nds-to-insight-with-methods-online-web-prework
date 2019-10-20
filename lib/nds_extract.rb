ca6516fe695a52856330aa4f90be8c64b27d6a04
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-20 01:54:43 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,11 +6,13 @@ require 'directors_database'
 # { directorOne => allTheMoneyTheyMade, ... }
 
 def directors_totals(nds)
-  result = {}
-  nil
+  result = {:name=>"Test Director",:movies=>[]}
+  
+ 
 end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  binding.pry
 end
