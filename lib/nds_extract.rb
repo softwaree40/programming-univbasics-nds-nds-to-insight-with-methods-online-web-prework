613ebab03ae6a3216367fe4b18ff929513a94cb3
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-20 02:15:06 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -14,13 +14,8 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-    total_result =0
-    i = 0 
-    while i < director_data.length 
-         value = director_data[i]
-         puts value
- 
-      i+=1
+    director_data.each do |k ,v|
+      puts k
       
     end
 end
